fab340089db49c0520cb8348440ad3f84bfcdc09
AleksKen <aleks.ken2017@mail.ru>
2025-02-24 20:25:54 +0300

    Докер

--- app/src/main/kotlin/com/example/app/AppApplication.kt
+++ app/src/main/kotlin/com/example/app/AppApplication.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AppApplication
-
-fun main(args: Array<String>) {
-	runApplication<AppApplication>(*args)
+class AppApplication {
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			runApplication<AppApplication>(*args)
+		}
+	}
 }
